1a752101848d06f0d7283839d49753c1bae7b425
vishal jn <vishaljn.25@ichat.sp.edu.sg>
2026-07-21 13:53:58 +0800

    removed block

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ file_path = r"C:\Local_Git_Repository\MLAI_project\Resaleflatprices.csv"
 df = pd.read_csv(file_path)
 new_df=df.sample(n=50000, random_state=42)
 del new_df["street_name"]
+del new_df["block"]
 print(new_df)
 print(np.shape(new_df))
-
+print(new_df.isna().sum())
